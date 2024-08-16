a09ca36393fa2afae18508d17793efbe9317b7fa
sawankumar-pal-au9 <manojpal211999@gmail.com>
2024-08-16 21:21:33 +0530

    add the vertical line left side

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import Carousel from '../../component/Carousel/Carousel'
-import About from '../../component/About/About'
-import Category from '../../component/Category/Category'
-import Services from '../../component/Services/Services'
-import Journey from '../../component/Journey/Journey'
-import Blog from '../../component/Blog/Blog'
-import Colours from '../../component/Colours/Colours'
+import React from 'react';
+import Carousel from '../../component/Carousel/Carousel';
+import About from '../../component/About/About';
+import Category from '../../component/Category/Category';
+import Services from '../../component/Services/Services';
+import Journey from '../../component/Journey/Journey';
+import Blog from '../../component/Blog/Blog';
+import Colours from '../../component/Colours/Colours';
 
 const Home = () => {
   return (
-    <div>
+    <div className="relative">
+      {/* Vertical Rainbow Gradient Line */}
+      <div
+        className="absolute left-0 top-0 h-full bg-gradient-to-b from-red-500 via-green-500 to-blue-500"
+        style={{ width: '8px' }}
+      ></div>
+
       <Carousel />
-      <About/>
+      <About />
       <Category />
       <Services />
       <Colours />
       <Journey />
       <Blog />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
